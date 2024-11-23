364fb727c9110979e4744d6088a907868d3d93b9
Abdelhamid Bouazi <abouazi@e2r4p8.1337.ma>
2024-11-23 16:27:50 +0100

    starting ex00

--- Python - 4/ex00/statistics.py
+++ Python - 4/ex00/statistics.py
@@ -1,2 +1,23 @@
-def ft_statistics(*args: Any, **kwargs: Any) -> None:
-    
+def check_args(args, kwargs):
+    keys = ["toto", "tutu", "tata"]
+    operations = ["mean", "median","quartile"]
+    for i in args:
+        if not isinstance(i, int):
+            return 0
+    for key in kwargs:
+        if not key in keys or not kwargs[key] in operations:
+            print(kwargs[key])
+            return 0
+    return 1
+
+
+def ft_statistics(*args: any, **kwargs: any) -> None:
+    try:
+        assert len(args) > 0, "ERROR"
+        assert check_args(args, kwargs), "ERROR"
+        # check_args(args, kwargs)
+        print(f"passed, args : {type(args)}, {type(kwargs)}")
+    except :
+        print("ERROR")
+        return
+ft_statistics(2, 5, 8, 5, 7, toto="mean", tutu="median", tata="quartile")
